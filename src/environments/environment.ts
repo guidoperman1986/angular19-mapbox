d4f0503ca4bb3bed0f33574b9e7ce36b3a9272ae
Guido Perman <guidoyaelperman@gmail.com>
2025-03-06 13:07:18 -0300

    chore: remove hardcoded Mapbox access token from environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 
 export const environment = {
-  mapbox_key: "pk.eyJ1IjoiZ3VpZG9wZXJtYW4iLCJhIjoiY2s0YW13dHRoMDNycjNlcGFuMmhubGJjNSJ9.Z92hBclh4KkowqgEPBmXZg",
+  mapbox_key: "",
   otra: "PROPIEDAD",
 };
